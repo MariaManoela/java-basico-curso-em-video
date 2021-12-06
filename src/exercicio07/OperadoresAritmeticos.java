c9175016ff340f5f6ca839fc9422ed2c1cbc5a19
Maria Manoela <manoelamvc@hotmail.com>
2021-12-06 11:37:01 -0300

    Adding main class

--- src/exercicio07/OperadoresAritmeticos.java
+++ src/exercicio07/OperadoresAritmeticos.java
@@ -1,4 +1,10 @@
 package exercicio07;
 
 public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        int n1 = 3;
+        int n2 = 5;
+        float media = (n1 + n2)/2;
+        System.out.println("A média é: " + media);
+    }
 }
