49b268a7cf73fcead11e48a4e0cb79467cde0a44
Maria Manoela <manoelamvc@hotmail.com>
2021-12-06 11:20:49 -0300

    Adding current year

--- src/exercicio06/Calculadora.java
+++ src/exercicio06/Calculadora.java
@@ -3,20 +3,24 @@ package exercicio06;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.Year;
 
 public class Calculadora {
     private JPanel panel1;
     private JButton btnCalcular;
     private JSpinner txtAnoNascimento;
     private JLabel lblIdade;
+    private JLabel lblAnoAtual;
 
     public Calculadora() {
         btnCalcular.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                int anoSistema = Year.now().getValue();
                 int anoNascimento = (int) txtAnoNascimento.getValue();
-                int idade = 2021 - anoNascimento;
+                int idade = anoSistema - anoNascimento;
                 lblIdade.setText(Integer.toString(idade));
+                lblAnoAtual.setText(Integer.toString(anoSistema));
             }
         });
     }
