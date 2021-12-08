5ea596e20c78acab16d248d7191cadc39aee0821
Maria Manoela <manoelamvc@hotmail.com>
2021-12-08 08:47:55 -0300

    Adding increment, decrement and assignment operators

--- src/exercicio07/OperadoresAritmeticos.java
+++ src/exercicio07/OperadoresAritmeticos.java
@@ -6,5 +6,16 @@ public class OperadoresAritmeticos {
         int n2 = 5;
         float media = (n1 + n2)/2;
         System.out.println("A média é: " + media);
+
+//        INCREMENTO / DECREMENTO
+        int numero = 10;
+        int valor = 4 + numero--;
+        System.out.println(valor);
+        System.out.println(numero);
+
+//        OPERADORES DE ATRIBUIÇÃO
+        int x = 4;
+        x += 2; // x = x + 2
+        System.out.println(x);
     }
 }
