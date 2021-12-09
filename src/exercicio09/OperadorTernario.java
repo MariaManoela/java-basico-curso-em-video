baf9453fb3bdf5c2d47ecae074cc0f61d87aa5ed
Maria Manoela <manoelamvc@hotmail.com>
2021-12-09 10:29:08 -0300

    Finishing application

---
+++ src/exercicio09/OperadorTernario.java
@@ -0,0 +1,11 @@
+package exercicio09;
+
+public class OperadorTernario {
+    public static void main(String[] args) {
+        int n1, n2, resultado;
+        n1 = 14;
+        n2 = 18;
+        resultado = (n1>n2)?(n1+n2):(n1-n2);
+        System.out.println(resultado);
+    }
+}
